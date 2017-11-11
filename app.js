cb391ade5376205817e31a1436f43cd48cc8f78c
Noah <noahmatsell@gmail.com>
2017-11-10 17:53:51 -0800

    added promises

--- app.js
+++ app.js
@@ -12,21 +12,38 @@ var T = new Twit({
   access_token_secret: config.access_token_secret,
   timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
 });
-
-T.get('users/lookup', { screen_name: 'nonojamesm' },  function (err, data, response) {
-  console.log(data)
-});
-
-//Your 5 most recent tweets.
-  //GET statuses / user_timeline
-  //Returns a collection of the most recent Tweets posted by the indicated by the screen_name or user_id parameters.
-//Your 5 most recent friends.
-//Your 5 most recent private messages.
-
 // app.use(bodyParser.urlencoded({ extended: false}));
 // app.use(cookieParser());
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+  Promise.all([
+    T.get('users/lookup', { screen_name: 'nonojamesm' }),
+    T.get('statuses/user_timeline', { screen_name: 'nonojamesm', count: 5 }),
+    T.get('friends/list', { count: 5 }),
+    T.get('direct_messages', { count: 5 })
+  ]).then( (userData, tweetData, friendData, dmData)=> {
+    console.log('promises complete');
+    console.log(userData[0].data);
+    console.log(tweetData.data);
+    console.log(friendData.data);
+    console.log(dmData.data);
+    
+    var templateData = {
+      "user": getUser(userData),
+      "tweets": tweetData(tweetData),
+      "friends": friendData(friendData),
+      "messages": dmData(dmData)
+    }
+    console.log(templateData);
+
+    res.render('app', { templateData });
+
+  }).catch(error => {
+    throw error;
+  });
+});
 
 app.use((request,res,next)=>{
   const err = new Error('Oh no!');
@@ -49,3 +66,59 @@ var port=Number(process.env.PORT || 3000);
 app.listen(port, () => {
     console.log('The application is running on localhost:3000');
 });
+
+
+function getUser(data){
+  console.log('getting user');
+  var user = data[0];
+  var newUser = {};
+  newUser.name = user.name; 
+  newUser.screen_name = user.screen_name;
+  newUser.profile_image_url_https = user.profile_image_url_https;
+  return newUser;
+};
+function getTweets(data){
+  console.log('getting tweets');
+  var tweets = data;
+  var allTweets = [];
+  tweets.forEach(function(tweet) {
+    var newTweet = {};
+    newTweet.user.name = tweet.user.name;
+    newTweet.user.screen_name = tweet.user.screen_name;
+    newTweet.user.profile_image_url_https = tweet.user.profile_image_url_https;
+    newTweet.text = tweet.text;
+    newTweet.favorite_count = tweet.favorite_count;
+    newTweet.retweet_count = tweet.retweet_count;
+    newTweet.created_at = tweet.created_at;
+    allTweets.push(newTweet);
+  });
+  return allTweets;
+};
+function getFriends(data){
+  console.log('getting friends');
+  var friends = data.users;
+  var allFriends = [];
+  friends.forEach(function(friend) {
+    var newFriend = {};
+    newFriend.name = friend.name;
+    newFriend.screen_name = friend.screen_name;
+    newFriend.profile_image_url_https = friend.profile_image_url_https;
+    allFriends.push(newFriend);
+  });
+  return allFriends;
+};
+function getMessages(data){
+  console.log('getting messages');
+  var messages = data;
+  var allMessages = [];
+  messages.forEach(function(message) {
+    var newMessage = {};  
+    newMessage.text = message.text;
+    newMessage.created_at = message.created_at;
+    newMessage.sender.name = message.sender.name;
+    newMessage.sender.screen_name = message.sender.screen_name;
+    newMessage.sender.profile_image_url_https = message.sender.profile_image_url_https;
+    allMessages.push(newMessage);
+  });
+  return allMessages;
+};
